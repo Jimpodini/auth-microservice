5b99cc9a67ef7c7ddab9e69cc57fcb6c54fa289d
Schnacken <e.schnackenburg@gmail.com>
2020-01-28 20:55:55 +0100

    Add useful packages and test them

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
+const morgan = require('morgan');
+const config = require('config');
+
+console.log(config.get('name'));
+console.log(config.get('mail.server'));
+console.log(config.get('mail.password'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(helmet());
+
+if (app.get('env') === 'development') {
+    console.log("morgan enabled...");
+    app.use(morgan('tiny'));
+}
 
 app.get('/', (req, res) => {
     res.send("Hello world");
